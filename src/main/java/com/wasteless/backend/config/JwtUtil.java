1c68340426389ce00f5764255c3d46710a09ee23
opratem <praiseolufemi1@gmail.com>
2025-12-12 06:08:33 +0100

    dev

--- src/main/java/com/wasteless/backend/config/JwtUtil.java
+++ src/main/java/com/wasteless/backend/config/JwtUtil.java
@@ -16,10 +16,10 @@ import java.util.function.Function;
 @Component
 public class JwtUtil {
 
-    @Value("${jwt.secret:YourSuperSecretKeyThatShouldBeLongEnoughForHS256}")
+    @Value("${JWT_SECRET:${jwt.secret:YourSuperSecretKeyThatShouldBeLongEnoughForHS256}}")
     private String SECRET_KEY;
 
-    @Value("${jwt.expiration:86400000}")
+    @Value("${JWT_EXPIRATION:${jwt.expiration:86400000}}")
     private long EXPIRATION_TIME;
 
     private Key getSignKey() {
